e12790af5f2195623510523f691f34718aa81784
Minositu <43652017+Minositu@users.noreply.github.com>
2024-06-22 13:04:31 +0200

    Initial Commit

---
+++ Executable_src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <dothing.h>
+
+int main()
+{
+    printf("HelloWorld!\n");
+    dothing(0);
+    return 0;
+}
---
+++ Static_src/dothing.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int dothing(int thing)
+{
+    std::cout << thing << " been done!" << std::endl;
+    return thing + 1;
+}
